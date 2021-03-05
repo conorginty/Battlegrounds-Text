ed4a28bd183386622993bced3bbf5173564ce602
Conor Ginty <conorginty@gmail.com>
2021-03-05 22:59:23 +0000

    Same as last commit (forgot to include) - stores an array of all the tier1 minions (so can be used to generate random instances for the tavern)

---
+++ src/main/java/com/conorginty/battlegrounds/Cards/Tier1Minions/AllTier1Minions.java
@@ -0,0 +1,12 @@
+package com.conorginty.battlegrounds.Cards.Tier1Minions;
+
+import com.conorginty.battlegrounds.Cards.Minion;
+
+public class AllTier1Minions {
+    // All the Minion Instances (in Array Lists for easy access)
+    public static Minion[] tier1Minions = {
+            new MurlocTidehunter(null),
+            new AlleyCat(null),
+            new FiendishServant(null)
+    };
+}
